4229c5a38615928c7c4fec73c52d285eb1fd9000
Diana Chacon <dcmedina@gmail.com>
2020-04-11 12:58:58 -0700

    202004-Diana_dropdown_bubble_start

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-url="https://opendata.arcgis.com/datasets/4a702cd67be24ae7ab8173423a768e1b_0.geojson"
+url = "https://opendata.arcgis.com/datasets/4a702cd67be24ae7ab8173423a768e1b_0.geojson"
 
 var myMap = L.map("map", {
   center: [34.052235, -118.243683],
@@ -14,17 +14,68 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-d3.json(url, function(response) {
+var onlineyear
+var energy_type
+var MWproduction
+var stationID
 
- 
-  console.log(response)
-  for (var i = 0; i < response.length; i++) {
-    var location = response[i].location;
+// Grab geoJson data and create the map
+d3.json(url, function (response) {
+  L.geoJson(response, {
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup("Plant Label: " + feature.properties.Plant_Label + "<br>");
+      // console.log(response)
+      onlineyear = feature.properties.Online_Year;
+      energy_type = feature.properties.General_Fuel;
+      MWproduction = feature.properties.MW;
+      stationID = feature.properties.Plant_ID;
 
-
-    if (location) {
-      L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
+      // var yticks = count(stationID).groupby(energy_type, onlineyear)
+      // console.log(onlineyear)
     }
-  }
 
+
+  }).addTo(myMap)
 });
+
+function createBubblechart(onlineyear, energy_type, MWproduction, stationID) {
+
+  // L.geoJson(data, {
+  //   onEachFeature: function (feature) {
+  //     // var onlineyear = feature.properties.Online_Year;
+  //     // var energy_type = feature.properties.General_Fuel;
+  //     // var MWproduction = feature.properties.MW;
+  //     // var stationID = feature.properties.Plant_ID;
+
+  //     // var yticks = count(stationID).groupby(energy_type, onlineyear)
+
+  //     console.log(data)
+  //   }
+  // })
+
+  var trace2 = {
+
+    x: onlineyear,
+    y: MWproduction,
+    mode: "markers",
+    marker: {
+      size: MWproduction,
+      color: energy_type
+    }
+  };
+
+  var trace2data = [trace2]
+
+  var layout2 = {
+    title: "Historical Energy Production",
+    margin: { t: 0 },
+    hovermode: "closest",
+    xaxis: { title: "Online Year" },
+    margin: { t: 30 }
+  };
+
+
+  Plotly.newPlot("bubble", trace2data, layout2);
+
+};
+createBubblechart(onlineyear, energy_type, MWproduction, stationID)
